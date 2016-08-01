52e7051c15a6d36e9792f57a4f44d5b2c84550ae
alvin562 <alvindantic562@gmail.com>
2016-08-01 01:19:24 -0700

    current solution to Lesson 6 - Distinct, still needs fixing (did not pass all test cases)

---
+++ Distinct.py
@@ -0,0 +1,59 @@
+# you can write to stdout for debugging purposes, e.g.
+# print "this is a debug message"
+    
+
+
+def quickSort(A, start, end):
+    
+    if start >= end:
+        return
+    
+    wall = start - 1    # we place the wall one position behind the start
+    curr = start        # position of current element is initially pointing to start
+    pivot = A[end]      # our pivot is always the last element
+    
+    while curr < end:
+        
+        # if the current element is less than the pivot
+        # then we swap it with the element to the right
+        # of the wall and move wall up one spot
+        if A[curr] < pivot:
+            
+            temp = A[wall+1]
+            A[wall+1] = A[curr]
+            A[curr] = temp
+            
+            wall += 1
+        
+        curr += 1
+    
+    # finally we swap the pivot with the
+    # element just to the right of the wall
+    temp = A[wall+1]
+    A[wall+1] = A[end]
+    A[end] = temp
+    
+    # quicksort left and right halves of array
+    quickSort(A, start, wall)
+    quickSort(A, wall+2, end)
+        
+            
+
+def solution(A):
+
+    if len(A) <= 1:
+        return len(A)
+
+    answer = 0
+    total = A[0]
+
+    quickSort(A, 0, len(A)-1)
+    
+    for i in range(len(A)):
+        
+        if total != A[i]:
+            
+            total = A[i]
+            answer += 1
+            
+    return answer
