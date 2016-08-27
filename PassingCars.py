a741a6e12b5647f5c53f8205df944f306de8ad90
alvin562 <alvindantic562@gmail.com>
2016-08-26 23:48:47 -0700

    solution to Lesson 5 - PassingCars

--- PassingCars.py
+++ PassingCars.py
@@ -5,7 +5,7 @@ def solution(A):
     # write your code in Python 2.7
     
     east = 0    # total cars travelling east
-    west = 0    # total cars travelling west
+    west = 0    # 1 if car is travelling west, 0 otherwise
     passingCars = 0 # total passing cars
     
     totalCars = len(A)
@@ -22,21 +22,22 @@ def solution(A):
         # else the car is heading in west direction so we increment west
         else:
             
-            west += 1
+            west = 1
+            
+            # passing cars are the number of cars going east 
+            # times the number of cars going west
+            passingCars += east 
             
             # if the current car is going west and the next car is going east
             # then we reset west to 0 since a car going east followed by a car
             # going west will never pass each other
             if i != totalCars-1 and A[i+1] == 0:
                 
-                west = 0
-            
-        # passing cars are the number of cars going east 
-        # times the number of cars going west
-        passingCars += east*west    
+                west = 0 
         
         if passingCars > 1000000000:
             
             return -1
             
     return passingCars
+
